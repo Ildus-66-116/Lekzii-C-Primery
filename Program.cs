89143ddbb0d8756738d5b8bf23878f75d989b90e
ILDUS <ildus6696@gmail.com>
2023-06-16 14:34:39 +0300

    Lekcia 6

--- Program.cs
+++ Program.cs
@@ -243,3 +243,24 @@
 //             .Where(e=> e>0)
 //             .Select(e=> new{q = e, w = e+1});
 // Console.WriteLine(data.GetType());
+
+// Лекция 6
+
+using System.Linq;
+Console.Clear();
+string text = "(1,2) (2,3) (4,5) (9,7)"
+                .Replace("(", "")
+                .Replace(")", "");
+Console.WriteLine(text);
+var data = text.Split(" ")
+                .Select(item => item.Split(','))
+                .Select(e => (x: int.Parse(e[0]), y: int.Parse(e[1])))
+                .Where(e => e.x % 2 == 0)
+                .Select(point => (point.x * 10, point.y + 10))
+                .ToArray();
+for(int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
+
+
